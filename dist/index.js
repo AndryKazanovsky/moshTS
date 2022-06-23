e69808636dfdfa0464cffa087bdab2624f164875
AndryKazanovsky <kazanovsky.andrew@gmail.com>
2022-06-23 19:59:13 +0300

    Mosh TS tsconfig

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('hello world');
+let age = 20;
+let str = 'ololo';
+if (age < 50) {
+    age += 10;
+}
+console.log(age);
+let user = [1, 'John'];
+console.log(user[0]);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log('hello world')
+let age = 20;
+let str = 'ololo';
+if (age < 50) { age += 10;}
+    
+console.log(age)
+
+let user: [number, string] = [1, 'John'];
+console.log(user[0])
